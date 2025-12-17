f2491bcd8654aeb72d9f44ee0437679522c1b572
solehasiddique <solehasiddique07@gmail.com>
2025-12-17 17:51:35 +0530

    Prepare for Vercel deployment with MongoDB Atlas

--- lib/connectDB.ts
+++ lib/connectDB.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
+  if (mongoose.connection.readyState >= 1) return; // reuse connection
   await mongoose.connect(process.env.MONGO_URI!);
   console.log("✅ MongoDB connected");
 };
 
 export default connectDB;
-
