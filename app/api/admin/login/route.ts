3a075e33a59e4c172c9fcd3acafb82bc06600421
solehasiddique <solehasiddique07@gmail.com>
2026-04-18 02:12:00 +0530

    debug

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -4,11 +4,13 @@ export async function POST(req: Request) {
   const { email, password } = await req.json();
 
   if (
-    email === process.env.ADMIN_EMAIL &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
+  email.trim().toLowerCase() === process.env.ADMIN_EMAIL?.trim().toLowerCase() &&
+  password.trim() === process.env.ADMIN_PASSWORD?.trim()
+) {
     const res = NextResponse.json({ success: true });
 
+    console.log("ENV:", process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD);
+console.log("INPUT:", email, password);
     // cookie
     res.cookies.set("admin_token", "authorized", {
       httpOnly: true,
